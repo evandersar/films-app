1e661437f5f22312491e1e9ec87add30310c4700
i_zinchenko <ivanz@aurelon.com>
2018-02-22 14:01:35 +0200

    Film class refactoring

--- src/app/film.ts
+++ src/app/film.ts
@@ -1,11 +1,12 @@
 export class Film {
-    public title : string;
+    /* public title : string;
     public episode_id : number;
     public director : string;
     public producer : string;
-    public release_date : string;
+    public release_date : string; */
+    /* constructor(title, episode_id, director, producer, release_date){ */
 
-    constructor(title, episode_id, director, producer, release_date){
+    constructor(public title : string, public episode_id : number, public director : string, public producer : string, public release_date : string){
         this.title = title;
         this.episode_id = episode_id;
         this.director = director;
